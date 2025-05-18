256964d908a08ddef57a4ab7c4ae8aaca7366260
didiermenard <dmenard@unistra.fr>
2025-05-18 20:38:34 +0200

    Update scan_trading_court_terme.py

--- scan_trading_court_terme.py
+++ scan_trading_court_terme.py
@@ -1,17 +1,14 @@
 import os
 import smtplib
 from email.mime.text import MIMEText
-from dotenv import load_dotenv
-
-load_dotenv()
 
 EMAIL_EXPEDITEUR = os.getenv("EMAIL_EXPEDITEUR")
 EMAIL_MDP = os.getenv("EMAIL_MDP")
 EMAIL_DESTINATAIRE = os.getenv("EMAIL_DESTINATAIRE")
 
 # Envoi test
-msg = MIMEText("🚀 Ceci est un test d'envoi automatique via Render.com")
-msg['Subject'] = "📈 Alerte de test Render"
+msg = MIMEText("📬 Ceci est un test d'envoi automatique via Render.com")
+msg['Subject'] = "📢 Alerte de test Render"
 msg['From'] = EMAIL_EXPEDITEUR
 msg['To'] = EMAIL_DESTINATAIRE
 
@@ -21,4 +18,8 @@ try:
         server.send_message(msg)
         print("✅ Email envoyé avec succès.")
 except Exception as e:
-    print(f"❌ Erreur d'envoi : {e}")
+    print(f"❌ Erreur d’envoi : {e}")
+
+print("EMAIL_EXPEDITEUR =", EMAIL_EXPEDITEUR)
+print("EMAIL_DESTINATAIRE =", EMAIL_DESTINATAIRE)
+
